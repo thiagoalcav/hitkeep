654020242e54fba68a95955c40db67d1528806ea
Pascale Beier <mail@pascalebeier.de>
2026-01-19 01:34:58 +0100

    chore: Fix wrong log level for nsq

--- internal/hklog/nsq_private.go
+++ internal/hklog/nsq_private.go
@@ -9,11 +9,21 @@ func parseNSQLevel(s string) slog.Level {
 	switch {
 	case strings.HasPrefix(s, "DBG"):
 		return slog.LevelDebug
+	case strings.HasPrefix(s, "DEBUG:"):
+		return slog.LevelDebug
 	case strings.HasPrefix(s, "INF"):
+		return slog.LevelInfo
+	case strings.HasPrefix(s, "INFO:"):
 		return slog.LevelInfo
 	case strings.HasPrefix(s, "WRN"):
 		return slog.LevelWarn
+	case strings.HasPrefix(s, "WARN:"):
+		return slog.LevelWarn
+	case strings.HasPrefix(s, "WARNING:"):
+		return slog.LevelWarn
 	case strings.HasPrefix(s, "ERR"):
+		return slog.LevelError
+	case strings.HasPrefix(s, "ERROR:"):
 		return slog.LevelError
 	default:
 		return slog.LevelInfo
